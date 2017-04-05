fc7b745a6f77b2a2049f813831159c7ffffacd7c
Duncan Fraiser <duncan.fraiser@ergon.com>
2017-04-05 14:11:00 -0500

    added info to page

--- resources/views/invitee/create.blade.php
+++ resources/views/invitee/create.blade.php
@@ -7,6 +7,7 @@
     {{-- <center><h1 style="color:red">Registration Opens April 12, 2017</h1></center> --}}
     	 <br/><center><a href="{{url('/')}}"><BUTTON class="ybfbtn">Home</BUTTON></a></center>
 	<div class="col-md-8 col-md-offset-2" style="margin-bottom: 3em">
+    <p><center><strong>Ergon's Classic Car, Truck, Motorcycle & Tractor Show<br/> Saturday, May 20, 2017<br/>9 – 11 a.m.<br/><br/>Setup begins at 9 a.m. and winners will be announced at 11 a.m.</strong></center></p></div>
 	  {{Form::open(['action' => 'InviteeController@store'])}}
 	    <div class="form-group">
 
@@ -14,12 +15,14 @@
 	        {{Form::label('invitee_email', "Please Enter Invitee Email Address", ['class' => 'errs'])}}<br/>
 	        {{Form::text('invitee_email', null, ['class' => 'redform-control', 'placeholder' => "Please Enter Invitee Email Address"])}} 
 	      @else
+	        {{Form::label('invitee_email', "Invitee Email Address")}}<br/>
 	        {{Form::text('invitee_email', null, ['class' => 'form-control', 'placeholder' => "Please Enter Invitee Email Address"])}}
 	      @endif
 	      @if($errors->has('employee_email'))	
 	        {{Form::label('employee_email', "Please Enter Employee Email Address", ['class' => 'errs'])}}<br/>
 	        {{Form::text('employee_email', null, ['class' => 'redform-control', 'placeholder' => "Please Enter Employee Email Address"])}} 
 	      @else
+	        {{Form::label('employee_email', "Employee Email Address")}}<br/>
 	        {{Form::text('employee_email', null, ['class' => 'form-control', 'placeholder' => "Please Enter Employee Email Address"])}} 
 	      @endif
 	    </div>
