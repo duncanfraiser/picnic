a98fa8c32136d37bb73efa07d77a5090b8a81cb0
Duncan Fraiser <duncan.fraiser@ergon.com>
2017-04-06 13:54:35 -0500

    fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
                        <a href="{{url('carshow/create')}}"><button style="margin-bottom: 1em" class="ybbtn">Auto, Motorcycle & Tractor Show Registration</button></a>           
                   </div>
                   <div class="col-md-4">
-                       <a href="{{url('invitee/create')}}"><button style="margin-bottom: 1em" class="ybbtn">Invite a Friend to the Auto, Motorcycle & Tractor Show</button></a>           
+                       <a href="{{url('invitee/create')}}"><button style="margin-bottom: 1em" class="ybbtn">Invite a Friend to enter their Auto, Motorcycle or Tractor</button></a>           
                   </div>
                 
 
